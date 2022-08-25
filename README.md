0448c47cf65227357f04e88b6c4497b2844534b0
onelcruz66 <onel.cruz@twosixtech.com>
2022-08-25 13:06:27 -0400

    Implement PostgreSQL database

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # data_collector_app
+Personal web form project built with the Flask framework that takes user data, and stores it into a PostgresSQL database.
+
--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template, request
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234qwer@localhost/data_collector_app'
+db = SQLAlchemy(app)
+class Data(db.Model):
+    __tablename__="user_data"
+    id=db.Column(db.Integer, primary_key=True)
+    fname_=db.Column(db.String(50))
+    lname_=db.Column(db.String(50))
+    email_=db.Column(db.String(120), unique=True)
+    phone_=db.Column(db.BIGINT)
+    height_=db.Column(db.Integer)
+    weight_=db.Column(db.Integer)
+
+    def __init__(self, fname_, lname_, email_, phone_, height_, weight_):
+        self.fname_=fname_
+        self.lname_=lname_
+        self.email_=email_
+        self.phone_=phone_
+        self.height_=height_
+        self.weight_=weight_
+
+
 
 @app.route("/")
 
@@ -12,9 +33,16 @@ def index():
 
 def success():
     if request.method == 'POST':
+        fname = request.form["first_name"]
+        lname = request.form["last_name"]
         email = request.form["email_name"]
+        phone = request.form["phone_number"]
         height = request.form["height_amount"]
-        print(email, height)
+        weight = request.form["weight_amount"]
+        data = Data(fname, lname, email, phone, height, weight)
+        print(email, height, fname, lname, phone, height, weight)
+        db.session.add(data)
+        db.session.commit()
     return render_template("success.html")
 
 if __name__ == '__main__':
